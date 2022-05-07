5e833b1910e535c9f6b02adaed7ebc2f1e0ebd11
Anita Gyurdzhyan <anita088@mail.ru>
2022-05-08 00:01:20 +0400

    add day 1

---
+++ src/main/java/day1/Task1.java
@@ -0,0 +1,13 @@
+// Вывести на экран слово “JAVA”, в строку, чтобы оно повторилось 10 раз, используя цикл while.
+package day1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a = 0;
+        while (a < 10) {
+            System.out.print("JAVA ");
+            a++;
+
+        }
+    }
+}

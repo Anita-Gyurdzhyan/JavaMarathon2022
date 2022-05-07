baddf24d045474ce960c20fc67b89adcb42a67c8
Anita Gyurdzhyan <anita088@mail.ru>
2022-05-08 00:25:31 +0400

    add day 3

---
+++ src/main/java/day3/Task2.java
@@ -0,0 +1,33 @@
+
+// Реализовать программу, которая пока работает, принимает на вход от пользователя два числа - делимое и делитель.
+// Для этих двух чисел программа рассчитывает результат деления и выводит его в консоль.
+// Программа останавливает свою работу тогда, когда пользователь вводит 0 в качестве делителя.
+//(для этих вещественных чисел необходимо использовать тип double и метод nextDouble() у Scanner’а соответственно).
+// В решении обязательно использовать оператор break.
+
+package day3;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Введите два числа ");
+            double numberFirst = scanner.nextDouble();
+            double numberSecond = scanner.nextDouble();
+            double result = numberFirst / numberSecond;
+
+            if (numberSecond == 0) {
+                break;
+
+            } else {
+                System.out.println("Результат от деления " + result);
+                continue;
+            }
+
+
+        }
+    }
+}

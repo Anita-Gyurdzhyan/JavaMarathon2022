99e1c1d52aa3f4c3c7351548bdcefe295273f825
Anita Gyurdzhyan <anita088@mail.ru>
2022-08-31 00:06:34 +0400

    add day 9 task 2

--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -2,6 +2,48 @@ package day9.Task2;
 
 public class TestFigures {
     public static void main(String[] args) {
+        Triangle triangle = new Triangle(10, 20, 30, "green");
+
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+
+        System.out.println("Сумма периметров красных фигур " + TestFigures.calculateRedPerimeterfor(figures));
+        System.out.println("Сумма площадей красных фигур " + TestFigures.calculateRedArea(figures));
 
     }
+
+    public static double calculateRedPerimeterfor (Figure[] figures) {
+        double sumPerRed = 0;
+
+        for (int i = 0; i < figures.length; i++) {
+            if (figures[i].getColor() == "Red"){
+                sumPerRed +=  figures[i].perimeter();
+            }
+        }
+        return sumPerRed;
+
+    }
+
+
+    public static double calculateRedArea(Figure[] figures){
+        double sumAreaRed = 0;
+
+        for (int i = 0; i < figures.length; i++) {
+            if (figures[i].getColor() == "Red"){
+                sumAreaRed += figures[i].area();
+            }
+
+        }
+        return sumAreaRed;
+    };
 }
+
+//Все ли корреткно в моем решении?
